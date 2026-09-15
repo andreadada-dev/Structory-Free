628a6288eb0f621a557b39cbdc09c4a5d3a1105d
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-15 21:05:02 +0200

    fix: persist saved items atomically with backup

--- structory-free/src/main/java/me/mrbast/structory/config/SingleSavedItemConfig.java
+++ structory-free/src/main/java/me/mrbast/structory/config/SingleSavedItemConfig.java
@@ -3,46 +3,47 @@ package me.mrbast.structory.config;
 import me.mrbast.dadaconfig.logic.Config;
 import me.mrbast.structory.Structory;
 import me.mrbast.structory.saveditem.SavedItemProvider;
+import me.mrbast.structory.util.AtomicFileUtil;
 import me.mrbast.structory.util.FileUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SingleSavedItemConfig extends Config {
 
     public static final Logger LOGGER = Structory.getPlugin(Structory.class).getLogger();
 
-    private SavedItemProvider savedItemProvider;
-    private File file;
+    private final SavedItemProvider savedItemProvider;
+    private final File file;
 
     public SingleSavedItemConfig(SavedItemProvider savedItemProvider) {
         this.savedItemProvider = savedItemProvider;
-        this.file = FileUtil.prepare("customitem/"+savedItemProvider.getKey().getKey()+".yml", true);
+        this.file = FileUtil.prepare("customitem/" + savedItemProvider.getKey().getKey() + ".yml", false);
     }
-
 
     @Override
     public void load() {
-
-
-
-
     }
 
     @Override
     public void save() {
-        if(!file.exists())  return;
         write(SavedItemProvider.class, "", savedItemProvider);
         try {
-            super.save(file);
+            AtomicFileUtil.writeWithBackup(file, super::save);
         } catch (IOException e) {
-            LOGGER.warning("Couldn't save structure instance '"+savedItemProvider.getKey().getKey()+"' config");
+            LOGGER.log(Level.WARNING,
+                    "Couldn't save custom item '" + savedItemProvider.getKey().getKey() + "' config", e);
         }
     }
 
     public void delete() {
-        if(!file.exists()) return;
-        file.delete();
+        try {
+            AtomicFileUtil.deleteWithBackup(file);
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING,
+                    "Couldn't delete custom item '" + savedItemProvider.getKey().getKey() + "' config", e);
+        }
     }
 }
