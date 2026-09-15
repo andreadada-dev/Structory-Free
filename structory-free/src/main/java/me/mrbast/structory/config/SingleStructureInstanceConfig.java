537e377384ada449cf6b6daa29a5bc2c3a9db6ba
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-15 21:05:17 +0200

    fix: persist structure instances atomically with backup

--- structory-free/src/main/java/me/mrbast/structory/config/SingleStructureInstanceConfig.java
+++ structory-free/src/main/java/me/mrbast/structory/config/SingleStructureInstanceConfig.java
@@ -3,40 +3,46 @@ package me.mrbast.structory.config;
 import me.mrbast.dadaconfig.logic.Config;
 import me.mrbast.structory.Structory;
 import me.mrbast.structory.structure.StructureInstance;
+import me.mrbast.structory.util.AtomicFileUtil;
 import me.mrbast.structory.util.FileUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SingleStructureInstanceConfig extends Config {
     public static final Logger LOGGER = Structory.getPlugin(Structory.class).getLogger();
 
     private final StructureInstance structureInstance;
-    private File file;
+    private final File file;
+
     public SingleStructureInstanceConfig(StructureInstance structureInstance) {
         this.structureInstance = structureInstance;
-        this.file = FileUtil.prepare("instances/"+structureInstance.getData().getUUID()+".yml", true);
+        this.file = FileUtil.prepare("instances/" + structureInstance.getData().getUUID() + ".yml", false);
     }
 
     @Override
     public void load() {
-
     }
 
     @Override
     public void save() {
-        if(!file.exists())  return;
         write(StructureInstance.class, "", structureInstance);
         try {
-            super.save(file);
+            AtomicFileUtil.writeWithBackup(file, super::save);
         } catch (IOException e) {
-            LOGGER.warning("Couldn't save structure instance '"+structureInstance.getData().getUUID()+"' config");
+            LOGGER.log(Level.WARNING,
+                    "Couldn't save structure instance '" + structureInstance.getData().getUUID() + "' config", e);
         }
     }
 
     public void delete() {
-        if(!file.exists()) return;
-        file.delete();
+        try {
+            AtomicFileUtil.deleteWithBackup(file);
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING,
+                    "Couldn't delete structure instance '" + structureInstance.getData().getUUID() + "' config", e);
+        }
     }
 }
