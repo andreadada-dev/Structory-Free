4ba04a3af113905d47981ea0541c1ee329359c15
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-15 21:06:21 +0200

    fix: apply swappable checker logic with structure orientation

--- structory-free/src/main/java/me/mrbast/structory/structure/layout/checker/SwappableChecker.java
+++ structory-free/src/main/java/me/mrbast/structory/structure/layout/checker/SwappableChecker.java
@@ -1,28 +1,30 @@
 package me.mrbast.structory.structure.layout.checker;
 
+import me.mrbast.structory.structure.orientation.LevelOrientation;
+import me.mrbast.structory.structure.orientation.Orientation;
 import org.bukkit.Location;
 
-public class SwappableChecker extends Checker{
-
-
-    //TODO: FIX THIS
+public class SwappableChecker extends Checker {
 
     public SwappableChecker(int xOffset, int zOffset, BlockChecker check) {
         super(xOffset, zOffset, check);
     }
 
+    @Override
     public boolean check(Location center) {
-        return check.isValid(center.clone().add(xOffset, 0, zOffset))  || check.isValid(center.clone().subtract(zOffset, 0, xOffset));
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
+        return check(center, LevelOrientation.EAST);
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public boolean check(Location center, LevelOrientation levelOrientation) {
+        Orientation primary = levelOrientation.from(new Orientation(xOffset, zOffset, LevelOrientation.EAST));
+        if (check.isValid(center.clone().add(primary.getX().doubleValue(), 0, primary.getZ().doubleValue()))) {
+            return true;
+        }
+
+        // Preserve the legacy swappable transform: (x, z) -> (-z, -x),
+        // then rotate the transformed offset together with the whole structure.
+        Orientation swapped = levelOrientation.from(new Orientation(-zOffset, -xOffset, LevelOrientation.EAST));
+        return check.isValid(center.clone().add(swapped.getX().doubleValue(), 0, swapped.getZ().doubleValue()));
     }
 }
