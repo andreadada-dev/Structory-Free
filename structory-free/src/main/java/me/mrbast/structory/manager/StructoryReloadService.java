1d0a583e590772974c12d623539dd3f2761b773e
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 19:29:12 +0200

    fix: make Free reload a scheduler lifecycle barrier

--- structory-free/src/main/java/me/mrbast/structory/manager/StructoryReloadService.java
+++ structory-free/src/main/java/me/mrbast/structory/manager/StructoryReloadService.java
@@ -2,18 +2,23 @@ package me.mrbast.structory.manager;
 
 import me.mrbast.structory.async.StructureParticleScheduler;
 import me.mrbast.structory.crafting.option.CraftingOption;
+import me.mrbast.structory.util.SchedulerUtil;
 
 public final class StructoryReloadService {
     private StructoryReloadService() {
     }
 
     public static void reload() {
+        StructureParticleScheduler particleScheduler = StructureParticleScheduler.getInstance();
+        particleScheduler.stop();
+        SchedulerUtil.cancelPlatformTasks();
+
         CraftingOption crafting = CraftingOption.getInstance();
         crafting.dropAllRecipeItems();
         crafting.clearRuntimeState();
 
         OptionManager.getInstance().clear();
-        StructureParticleScheduler.getInstance().clear();
+        particleScheduler.clear();
 
         StructureInstanceManager.getInstance().clear();
         StructureManager.getInstance().clear();
@@ -22,5 +27,6 @@ public final class StructoryReloadService {
 
         OptionManager.getInstance().init();
         ConfigManager.getInstance().load();
+        particleScheduler.start();
     }
 }
