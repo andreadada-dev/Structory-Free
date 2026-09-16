819e9b3fe96e60710fac158c37c114907a9abf4d
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 13:05:17 +0200

    fix: make Free option lookups null-safe

--- structory-free/src/main/java/me/mrbast/structory/manager/OptionManager.java
+++ structory-free/src/main/java/me/mrbast/structory/manager/OptionManager.java
@@ -2,52 +2,51 @@ package me.mrbast.structory.manager;
 
 import me.mrbast.structory.crafting.option.CraftingOption;
 import me.mrbast.structory.enums.StructureSpacedKey;
-import me.mrbast.structory.option.*;
+import me.mrbast.structory.option.FireworksOption;
+import me.mrbast.structory.option.HasInstanceData;
+import me.mrbast.structory.option.NotifyOption;
+import me.mrbast.structory.option.Option;
+import me.mrbast.structory.option.ParticleOption;
 import org.bukkit.NamespacedKey;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 public class OptionManager {
 
-
     public static final OptionManager instance = new OptionManager();
 
-    public static OptionManager getInstance() {return instance;}
-
+    public static OptionManager getInstance() {
+        return instance;
+    }
 
     private final Map<NamespacedKey, Option> options = new HashMap<>();
     private final Map<String, NamespacedKey> stringToKey = new HashMap<>();
     private final Set<HasInstanceData> hasInstanceDataSet = new HashSet<>();
 
-    private OptionManager(){
-
+    private OptionManager() {
         registerOption(StructureSpacedKey.OPTION_FIREWORK.getNamespacedKey(), new FireworksOption());
         registerOption(StructureSpacedKey.OPTION_PARTICLE.getNamespacedKey(), ParticleOption.getInstance());
         registerOption(StructureSpacedKey.OPTION_CRAFTING.getNamespacedKey(), CraftingOption.getInstance());
         registerOption(StructureSpacedKey.OPTION_NOTIFY.getNamespacedKey(), NotifyOption.getInstance());
-
     }
 
-
-
-
-
-
     public void registerOption(NamespacedKey key, Option option) {
-        this.options.put(key, option);
-        this.stringToKey.put(key.getKey(), key);
+        options.put(key, option);
+        stringToKey.put(key.getKey(), key);
         ListenerManager.getInstance().subscribe(option);
-        try{
-            HasInstanceData instanceData  = (HasInstanceData) option;
-            hasInstanceDataSet.add(instanceData);
-        }catch (ClassCastException e){
-
+        if (option instanceof HasInstanceData) {
+            hasInstanceDataSet.add((HasInstanceData) option);
         }
     }
 
     public Optional<Option> getOption(String keyStr) {
+        if (keyStr == null) return Optional.empty();
         NamespacedKey key = stringToKey.get(keyStr);
-        return Optional.of(options.get(key));
+        return key == null ? Optional.empty() : Optional.ofNullable(options.get(key));
     }
 
     public Set<HasInstanceData> getHasInstanceData() {
@@ -59,11 +58,10 @@ public class OptionManager {
     }
 
     public void init() {
-
         options.values().forEach(Option::init);
-
     }
 
+    /** Option definitions are static for the Free edition; runtime caches are cleared by their owners. */
     public void clear() {
     }
 }
