91015f6a0b363e4d3347a4e24d4363c3b21a9a33
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 13:20:34 +0200

    fix: ignore backup files when loading saved items

--- structory-free/src/main/java/me/mrbast/structory/config/DirectorySavedItemConfig.java
+++ structory-free/src/main/java/me/mrbast/structory/config/DirectorySavedItemConfig.java
@@ -1,12 +1,9 @@
 package me.mrbast.structory.config;
 
-import me.mrbast.dadaconfig.logic.Config;
 import me.mrbast.dadaconfig.logic.VersionableConfig;
 import me.mrbast.structory.Structory;
 import me.mrbast.structory.manager.SavedItemManager;
-import me.mrbast.structory.manager.StructureInstanceManager;
 import me.mrbast.structory.saveditem.SavedItemProvider;
-import me.mrbast.structory.structure.StructureInstance;
 
 import java.util.Optional;
 
@@ -17,21 +14,16 @@ public class DirectorySavedItemConfig extends VersionableConfig {
 
     @Override
     public void load() {
+        initDirectory("customitem", true, file -> {
+            if (!file.getName().toLowerCase(java.util.Locale.ROOT).endsWith(".yml")) return;
 
-
-        initDirectory("customitem", true, file->{
-
-            try{
+            try {
                 Optional<SavedItemProvider> instance = file.read(SavedItemProvider.class);
-                instance.ifPresent(savedItemProvider -> {
-                    SavedItemManager.getInstance().register(savedItemProvider);
-                });
-            }catch (Exception e){
-                Structory.getPlugin(Structory.class).getLogger().severe("Error while loading '" + file.getName() + "' saved item file, it can be an error due to backporting the file from newer version");
+                instance.ifPresent(savedItemProvider -> SavedItemManager.getInstance().register(savedItemProvider));
+            } catch (Exception e) {
+                Structory.getPlugin(Structory.class).getLogger().severe(
+                        "Error while loading '" + file.getName() + "' saved item file, it can be an error due to backporting the file from newer version");
             }
-
-
         });
-
     }
 }
