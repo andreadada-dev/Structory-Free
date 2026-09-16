72cfa272798c305df5c919ef84a1845076ce20b1
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 19:19:54 +0200

    feat: expose runtime compatibility status in Free

--- structory-free/src/main/java/me/mrbast/structory/version/Version.java
+++ structory-free/src/main/java/me/mrbast/structory/version/Version.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
@@ -27,49 +28,66 @@ public abstract class Version {
 
     private static final Logger LOGGER = Structory.getPlugin(Structory.class).getLogger();
     private static Version instance;
-    private static List<VersionInstancer> versions = new ArrayList<>();
-    public static Version getInstance(){
-        return instance;
-    }
+    private static final List<VersionInstancer> versions = new ArrayList<>();
+    private static volatile String detectedBukkitVersion = "unknown";
+    private static volatile String selectedAdapter = "uninitialized";
+    private static volatile boolean explicitlySupported;
+
     static{
         versions.add(new VersionInstancer(Pattern.compile("1\\.(21.*|21|20.*|20|19.*|19)"), VersionLatest.class));
         versions.add(new VersionInstancer(Pattern.compile("1\\.(17.*|17|18.*|18)"), Version1718.class));
     }
 
+    public static Version getInstance(){
+        return instance;
+    }
 
+    public static String getDetectedBukkitVersion() {
+        return detectedBukkitVersion;
+    }
 
+    public static String getSelectedAdapter() {
+        return selectedAdapter;
+    }
 
+    public static boolean isExplicitlySupported() {
+        return explicitlySupported;
+    }
 
     private static class VersionInstancer {
-
         private final Pattern version;
         private final Class<?> classPath;
+
         public VersionInstancer(Pattern version, Class<?> classPath){
             this.version = version;
             this.classPath = classPath;
         }
-
     }
 
     public static void prepare(Structory plugin) throws RuntimeException{
+        detectedBukkitVersion = Bukkit.getServer().getBukkitVersion();
+        Optional<VersionInstancer> opt = versions.stream()
+                .filter(ver -> ver.version.matcher(detectedBukkitVersion).find())
+                .findFirst();
 
-        String version = Bukkit.getServer().getBukkitVersion();
-
-        Optional<VersionInstancer> opt = versions.stream().filter(ver -> ver.version.matcher(version).find()).findFirst();
-
+        explicitlySupported = opt.isPresent();
         if(opt.isPresent()){
-            LOGGER.info("Hooked to nms " + opt.get().classPath);
             try {
                 instance = (Version) opt.get().classPath.getDeclaredConstructor().newInstance();
+                selectedAdapter = opt.get().classPath.getSimpleName();
+                LOGGER.info("Version adapter: " + selectedAdapter + " for " + detectedBukkitVersion);
+                return;
             } catch (InstantiationException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
-                instance = new VersionDefault();
-                LOGGER.info("Hooked to default versioning, could probably break");
+                explicitlySupported = false;
+                LOGGER.log(Level.WARNING, "Unable to initialize explicit Structory version adapter", e);
             }
-        }else{
-            instance = new VersionDefault();
-            LOGGER.info("Hooked to default versioning, could probably break");
         }
 
+        instance = new VersionDefault();
+        selectedAdapter = VersionDefault.class.getSimpleName();
+        LOGGER.warning("Minecraft/Bukkit version '" + detectedBukkitVersion
+                + "' is outside Structory's explicit 1.17-1.21 compatibility matrix; using best-effort "
+                + selectedAdapter);
     }
 
     public abstract List<String> getComments(ConfigurationSection configSection, @NotNull String path);
@@ -78,25 +96,15 @@ public abstract class Version {
     public abstract void setInlineComments(ConfigurationSection configSection, @NotNull String path, @Nullable List<String> comments);
 
     public abstract boolean isClimbing(Player player);
-
     public abstract boolean isZero(Vector vector);
-
     public abstract PersistentDataType<Byte, Boolean> persistentDataTypeBoolean();
-    public abstract  <Z, T> boolean hasFilteredData(ItemStack item, NamespacedKey key, PersistentDataType<Z,T> type, Predicate<T> predicate);
+    public abstract <Z, T> boolean hasFilteredData(ItemStack item, NamespacedKey key, PersistentDataType<Z,T> type, Predicate<T> predicate);
     public abstract <Z,T> void setData(ItemStack itemStack, String test, PersistentDataType<Z, T> type, T i);
     public abstract <Z, T> boolean hasData(ItemStack item, NamespacedKey data, PersistentDataType<Z,T> type);
-
-
-
     public abstract void setSkullBlock(Block block, String texture);
     public abstract boolean isMainHand(PlayerInteractEvent event);
-
     public abstract ItemStack getSkinnedSkull(String skin);
-
     public abstract void playSound(Player player, Sound sound, float volume, float pitch);
     public abstract void playSound(Location location, Sound sound, float volume, float pitch);
-
     public abstract ItemStack getItemInMainHand(Player player);
-
-
 }
