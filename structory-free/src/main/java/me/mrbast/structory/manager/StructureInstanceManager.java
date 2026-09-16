30f0434aa587922f14150d5d25d7acaef43d5407
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 19:15:24 +0200

    fix: move structure instance deletion I/O off server thread

--- structory-free/src/main/java/me/mrbast/structory/manager/StructureInstanceManager.java
+++ structory-free/src/main/java/me/mrbast/structory/manager/StructureInstanceManager.java
@@ -3,11 +3,12 @@ package me.mrbast.structory.manager;
 import me.mrbast.structory.config.SingleStructureInstanceConfig;
 import me.mrbast.structory.structure.StructureInstance;
 import me.mrbast.structory.structure.grief.StructureProtection;
+import me.mrbast.structory.util.SchedulerUtil;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class StructureInstanceManager {
 
@@ -18,39 +19,25 @@ public class StructureInstanceManager {
 
     private StructureInstanceManager() {}
 
-
     public void register(StructureInstance instance) {
-
         structureInstances.put(instance.getData().getUUID(), instance);
-
     }
-
 
     public void clear(){
         structureInstances.clear();
     }
 
-
     public void delete(StructureInstance instance) {
-
         structureInstances.remove(instance.getData().getUUID());
         StructureProtection.getInstance().disable(instance);
-        new SingleStructureInstanceConfig(instance).delete();
-
+        SchedulerUtil.async(() -> new SingleStructureInstanceConfig(instance).delete());
     }
 
     public Collection<StructureInstance> getFiltered(Predicate<StructureInstance> filter) {
-
-
         return structureInstances.values().stream().filter(filter).collect(Collectors.toSet());
-
     }
 
     public Optional<StructureInstance> get(UUID uuid) {
         return Optional.ofNullable(structureInstances.get(uuid));
     }
-
-
-
-
 }
