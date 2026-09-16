6e4d1212864e299dafae3856a83935063bb1962a
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 13:21:17 +0200

    fix: reread message config from disk on reload

--- structory-free/src/main/java/me/mrbast/structory/manager/ConfigManager.java
+++ structory-free/src/main/java/me/mrbast/structory/manager/ConfigManager.java
@@ -10,14 +10,12 @@ public class ConfigManager {
     public static ConfigManager getInstance() { return INSTANCE; }
 
     public void load(){
-
         MainConfig.getInstance().load();
         DirectorySavedItemConfig.getInstance().load();
         RecipeConfig.getInstance().load();
         StructureConfig.getInstance().load();
         DirectoryStructureInstanceConfig.getInstance().load();
-        MessageConfig.getInstance().load();
+        MessageConfig.getInstance().reload();
         SchedulerUtil.getAsyncExecutor().init();
-
     }
 }
