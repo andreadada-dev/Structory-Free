e92a6b1f74e9a641f5ce9dfcff1e2bbb155604d2
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-16 13:15:09 +0200

    fix: clear Free firework option state on reload

--- structory-free/src/main/java/me/mrbast/structory/option/FireworksOption.java
+++ structory-free/src/main/java/me/mrbast/structory/option/FireworksOption.java
@@ -1,134 +1,72 @@
 package me.mrbast.structory.option;
 
 import me.mrbast.dadaconfig.logic.ConfigSection;
-import me.mrbast.structory.Structory;
 import me.mrbast.structory.enums.StructureSpacedKey;
-import me.mrbast.structory.event.StructureEventHandler;
 import me.mrbast.structory.event.AltarCreateEvent;
 import me.mrbast.structory.event.Listener;
+import me.mrbast.structory.event.StructureEventHandler;
 import me.mrbast.structory.structure.Structure;
 import me.mrbast.structory.structure.StructureInstance;
 import me.mrbast.structory.util.ColorUtil;
 import me.mrbast.structory.util.FireworkUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.Color;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
-import java.util.*;
-
-public class FireworksOption implements Option{
-
+public class FireworksOption implements Option {
 
     public interface FireworksSupplierToInstance {
-
         void spawn(StructureInstance instance);
-
     }
 
-
-    public static class Fireworks{
-
-
-        private FireworksSupplierToInstance fireworksSupplier;
-
+    public static class Fireworks {
+        private final FireworksSupplierToInstance fireworksSupplier;
 
         public Fireworks(int amount, int power, boolean flicker, Color[] mainColor, Color[] fadeColor) {
-
             fireworksSupplier = instance -> {
-                for(int i = 0; i < amount; i++) {
-                    FireworkUtil.prepareFirework(instance.getData().getCenter().clone().add(0.5, 1.5,0.5), 0, flicker, mainColor, fadeColor).detonate();
+                for (int i = 0; i < amount; i++) {
+                    FireworkUtil.prepareFirework(
+                            instance.getData().getCenter().clone().add(0.5, 1.5, 0.5),
+                            power,
+                            flicker,
+                            mainColor,
+                            fadeColor
+                    ).detonate();
                 }
             };
-
         }
     }
 
     private static final Map<Structure, Fireworks> structures = new HashMap<>();
 
-
-
-
     @Override
     public void read(Structure structure, ConfigSection section) {
+        Optional<String> type = section.read(String.class, "type");
+        int amount = section.read(Integer.class, "amount").orElse(0);
+        int power = section.read(Integer.class, "power").orElse(0);
+        boolean flicker = section.read(Boolean.class, "flicker").orElse(false);
+        Optional<String> colorsConfig = section.read(String.class, "colors");
+        Optional<String> fadeConfig = section.read(String.class, "fade");
 
-
-        Optional<String> optType = Optional.empty();
-        Optional<Integer> optAmount = Optional.empty();
-        Optional<Integer> optPower = Optional.empty();
-        Optional<String> optFade = Optional.empty();
-        Optional<String> optColors = Optional.empty();
-        Optional<Boolean> optFlicker = Optional.empty();
-
-        try{
-            optType = section.read(String.class, "type");
-            optAmount = section.read(Integer.class, "amount");
-            optPower = section.read(Integer.class, "power");
-            optFade = section.read(String.class, "fade");
-            optColors = section.read(String.class, "colors");
-            optFlicker = section.read(Boolean.class, "flicker");
-        }catch (Exception e){
-
-        }
-
-
-        final Optional<String> finalOptColors = optColors;
-        final Optional<String> finalOptFade = optFade;
-
-        int power;
-        power = optPower.orElse(0);
-
-        int amount;
-        amount = optAmount.orElse(0);
-
-        boolean flicker;
-        flicker = optFlicker.orElse(false);
-
-
-        optType.ifPresent(type->{
-
-            switch (type){
-
-                case "RANDOM":{
-
-
-                    Color[] colors = new Color[0];
-                    if(finalOptColors.isPresent()){
-                        String[] clrs = finalOptColors.get().split(", ");
-                        colors = new Color[clrs.length];
-                        for(int i = 0; i < clrs.length; i++){
-                            colors[i] = ColorUtil.getColor(clrs[i]);
-                        }
-                    }
-
-                    Color[] fades = new Color[0];
-                    if(finalOptFade.isPresent()) {
-                        String[] clrs = finalOptFade.get().split(", ");
-                        fades = new Color[clrs.length];
-                        for (int i = 0; i < clrs.length; i++) {
-                            fades[i] = ColorUtil.getColor(clrs[i]);
-                        }
-                    }
-
-
-                    String clrs = finalOptColors.orElse("");
-                    String fds = finalOptFade.orElse("");
-                    structures.put(structure, new Fireworks(amount, power, flicker, colors, fades));
-
-                }break;
-
-            }
-
-
+        type.filter("RANDOM"::equalsIgnoreCase).ifPresent(ignored -> {
+            Color[] colors = parseColors(colorsConfig.orElse(""));
+            Color[] fades = parseColors(fadeConfig.orElse(""));
+            structures.put(structure, new Fireworks(amount, power, flicker, colors, fades));
         });
+    }
 
-
-
-
+    private Color[] parseColors(String value) {
+        if (value == null || value.isBlank()) return new Color[0];
+        String[] raw = value.split(",\\s*");
+        Color[] result = new Color[raw.length];
+        for (int i = 0; i < raw.length; i++) result[i] = ColorUtil.getColor(raw[i]);
+        return result;
     }
 
     @Override
     public void write(ConfigSection configSection) {
-
     }
 
     @Override
@@ -137,28 +75,23 @@ public class FireworksOption implements Option{
     }
 
     private static final Listener eventListener = new Listener() {
-
-
         @StructureEventHandler
-        public void onEventCreate(AltarCreateEvent event){
-
-
+        public void onEventCreate(AltarCreateEvent event) {
             Fireworks fireworks = structures.get(event.getInstance().getData().getStructure());
-            if(fireworks == null) return;
-            fireworks.fireworksSupplier.spawn(event.getInstance());
-
+            if (fireworks != null) fireworks.fireworksSupplier.spawn(event.getInstance());
         }
-
     };
 
-
+    @Override
     public void init(Structure structure) {
-
-
     }
 
     @Override
     public void init() {
+    }
 
+    @Override
+    public void onDisable() {
+        structures.clear();
     }
 }
